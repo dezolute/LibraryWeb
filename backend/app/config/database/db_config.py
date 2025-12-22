3ad833f9157cb085776013a344e3f3471eb1229a
dezolute <voenikot@mail.ru>
2025-12-22 15:31:57 +0300

    deploy: edit config db

--- backend/app/config/database/db_config.py
+++ backend/app/config/database/db_config.py
@@ -7,14 +7,21 @@ class ConfigDB(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     POSTGRES_DB: str
+    SSL: bool = False
     ECHO: bool
 
     @property
     def database_url(self) -> str:
-        return (
-            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
-            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-        )
+        if self.SSL:
+            return (
+                f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}?sslmode=require"
+            )
+        else:
+            return (
+                f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+            )
 
     model_config = SettingsConfigDict(
         env_file="../.env",
